52bc3c3112af9ea3b2aae9691e80502e0298e7c3
James Wu <wu.james@hotmail.com>
2024-06-04 02:47:06 -0400

    correctly integrating tailwindcss with vue.js

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # epsilon
-a simple project using vue
+
+a simple project using vue, and for documenting my journey with vue
+
 ### development
+
 > npm run dev
 
 ### build for production
+
 > npm run build
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
